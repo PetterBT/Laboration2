af4e0b57fc10593e913da3e4edaffd2f03b1eb67
PetterBT <petter.bergstrom@iths.se>
2022-02-01 19:22:49 +0100

    Refactor code in Calculator.java

--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -7,38 +7,47 @@ import java.util.stream.Collectors;
 public class Calculator {
 
     static int add(String numbers) {
-        if (!numbers.isEmpty()) {
+       if (numbers.isEmpty())
+           return 0;
+       else {
+           List<Integer> numbersList = toInt(getSplit(numbers));
+           checkForNegativeNumbers(numbersList);
+           return getSum(numbersList);
+       }
+    }
+    private static String[] getSplit(String numbers) {
+        String delimiter = "[,\n]";
+        if (numbers.startsWith("//")) {
+            String[] parts = numbers.split("\n",2);
+            delimiter = parts[0].substring(2);
+            numbers = parts[1];
+        }
+        return numbers.split(delimiter);
+    }
 
-            String delimiter = "[,\n]";
-            if (numbers.startsWith("//")) {
-                String[] parts = numbers.split("\n",2);
-                delimiter = parts[0].substring(2);
-                numbers = parts[1];
-            }
-            String[] numbersArr = numbers.split(delimiter);
-            noNegativeNumbers(toInt(numbersArr));
-            return getSum(numbersArr);
-        }
-        else return 0;
-    }
-    private static void noNegativeNumbers(List<Integer> numbers) {
-        StringBuilder builder = new StringBuilder();
-        numbers.stream()
-                .filter(n -> n < 0)
-                .forEach(n -> builder.append(n).append(" "));
-        if (!builder.toString().isEmpty()) {
-            throw new RuntimeException("negatives not allowed " + builder.toString());}
-    }
-    private static int getSum(String[] numbersArr) {
-        int sum = 0;
-        for (String s : numbersArr) {
-            sum += Integer.parseInt(s);
-        }
-        return sum;
-    }
     private static List<Integer> toInt(String[] numbers) {
         return Arrays.stream(numbers)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    private static void checkForNegativeNumbers(List<Integer> numbers) {
+        StringBuilder builder = new StringBuilder();
+        numbers.stream()
+                .filter(n -> n < 0)
+                .forEach(n -> builder.append(n).append(" "));
+        printNegatives(builder);
+    }
+
+    private static void printNegatives(StringBuilder builder) {
+        if (!builder.toString().isEmpty()) {
+            throw new RuntimeException("negatives not allowed " + builder);}
+    }
+    private static int getSum(List<Integer> numbers) {
+        int sum = 0;
+        for (Integer n : numbers) {
+            sum += n;
+        }
+        return sum;
     }
 }
