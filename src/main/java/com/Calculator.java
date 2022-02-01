4feed0b7ac7da9d47bc3465ee8a2b6675c508024
PetterBT <petter.bergstrom@iths.se>
2022-02-01 13:58:42 +0100

    Refactor add-method to return sum of two numbers and add test

--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
     static int add(String numbers) {
         if (numbers.isEmpty())
         return 0;
+        else if (numbers.contains(",")) {
+            String[] numbersArr = numbers.split(",");
+            return Integer.parseInt(numbersArr[0]) + Integer.parseInt(numbersArr[1]);
+        }
         else {
             return Integer.parseInt(numbers);
         }
--- src/test/java/com/CalculatorTest.java
+++ src/test/java/com/CalculatorTest.java
@@ -12,9 +12,13 @@ public class CalculatorTest {
     }
     @Test
     void stringWithOneNumberShouldReturnNumber() {
-
         assertThat(Calculator.add("5")).isEqualTo(5);
         assertThat(Calculator.add("69")).isEqualTo(69);
+    }
+    @Test
+    void twoNumbersShouldReturnTheirsSum() {
+        assertThat(Calculator.add("2,6")).isEqualTo(8);
+        assertThat(Calculator.add("151,1349")).isEqualTo(1500);
     }
 
 }
