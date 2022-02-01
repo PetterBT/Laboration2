b99b9391758de0cbfb4d6511dd37865d4216f70f
PetterBT <petter.bergstrom@iths.se>
2022-02-01 13:38:24 +0100

    Refactor add-method to return passed value and add test

--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -3,6 +3,10 @@ package com;
 public class Calculator {
 
     static int add(String numbers) {
+        if (numbers.isEmpty())
         return 0;
+        else {
+            return Integer.parseInt(numbers);
+        }
     }
 }
--- src/test/java/com/CalculatorTest.java
+++ src/test/java/com/CalculatorTest.java
@@ -10,5 +10,9 @@ public class CalculatorTest {
     void emptyStringShouldReturnZero() {
         assertThat(Calculator.add("")).isEqualTo(0);
     }
+    @Test
+    void stringWithOneNumberShouldReturnNumber() {
+        assertThat(Calculator.add("69")).isEqualTo(69);
+    }
 
 }
